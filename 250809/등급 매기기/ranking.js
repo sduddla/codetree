5da79e509715d82a9e117674de39416a9dfef509
sy <sduddla921@gmail.com>
2025-08-09 04:11:42 +0900

    Title: 등급 매기기, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250809/등급 매기기/ranking.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let score = '';
+
+if(n >= 90) {
+    score = 'A';
+} else if(n >= 80) {
+    score = 'B';
+} else if (n >= 70) {
+    score = 'C';
+} else if (n >= 60) {
+    score = 'D';
+} else {
+    score = 'F';
+}
+
+console.log(score);

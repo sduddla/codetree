800fb0d6cbd2401ad39e335431ff421e18d782a8
sy <sduddla921@gmail.com>
2025-08-09 04:04:48 +0900

    Title: 살 수 있는 물건, Time: 177ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250809/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const ex = Number(fs.readFileSync(0).toString().trim());
+
+if(ex === 3000) {
+    console.log('book');
+} else if (ex === 1000) {
+    console.log('mask');
+} else {
+    console.log('no');
+}

02c30ff4a24d8eb613a27a0e047e9d39fa06bab2
sy <sduddla921@gmail.com>
2025-08-11 01:52:15 +0900

    Title: 점수 비교, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250811/점수 비교/score-comparison.js
+++ 250811/점수 비교/score-comparison.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const [a_math, a_eng, b_math, b_eng] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+const [a_math, a_eng, b_math, b_eng] = fs.readFileSync(0).toString().trim().split(/\s+/).map(Number);
 
-if(a_math > b_math) {
-    console.log(1);
-} else if(a_math > b_math && a_eng > b_eng) {
+if (a_math > b_math && a_eng > b_eng) {
     console.log(1);
 } else {
     console.log(0);

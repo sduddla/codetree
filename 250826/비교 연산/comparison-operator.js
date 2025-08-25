6af1d85b2ae323438cb03b50c54fb7eb6cb213fa
sy <sduddla921@gmail.com>
2025-08-26 02:23:05 +0900

    Title: 비교 연산, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250826/비교 연산/comparison-operator.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = input[0];
+let b = input[1];
+
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(a <= b ? 1 : 0);
+console.log(a < b ? 1 : 0);
+console.log(a === b ? 1 : 0);
+console.log(a !== b ? 1 : 0);

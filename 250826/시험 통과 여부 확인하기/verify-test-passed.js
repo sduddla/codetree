bd0a24583a5d50a4a2c82fe9f52d75bc334136e7
sy <sduddla921@gmail.com>
2025-08-26 02:27:03 +0900

    Title: 시험 통과 여부 확인하기, Time: 180ms, Memory: 14MB, Status: Runtime Error - Codetree

---
+++ 250826/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const score = Number(fs.readFileSync(0).toString().trim());
+
+if(score >= 80) {
+    console.log('pass');
+} else {
+    console.log(`${80-n} more score`);
+}
